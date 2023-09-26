cadf3e17c685d36751be040c534d0538a6b5042d
Heeyoung Yang <dev.heeyoung@gmail.com>
2023-09-25 20:20:00 -0400

    Responsive: 404 Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
     return (
-            <section className='flex flex-col justify-center items-center font-basic w-screen min-h-screen text-brand'>
-                <Link to='/' className='text-5xl mb-20 font-brand text-point font-semibold'>Heefify</Link>
-                <div className='opacity-80 mb-20'>
-                    <h1 className='font-brand font-semibold text-3xl mb-5'>Sorry, we couldn't find that page for you.</h1>
-                    <p className=''>Let us help you find something else and discover some helpful resources blow.</p>
+            <section className='flex flex-col text-center justify-center items-center font-basic text-brand mb-20 lg:h-screen lg:mb-0'>
+                <Link to='/' className='text-5xl mb-10 font-brand text-point font-semibold mt-10 lg:mt-0 lg:mb-20'>Heefify</Link>
+                <div className='opacity-80 mb-10 lg:mb-20'>
+                    <h1 className='font-brand font-semibold mb-2 text-xl lg:mb-5 lg:text-3xl'>Sorry, we couldn't find that page for you.</h1>
+                    <p className='text-sm md:text-base mx-5'>Let us help you find something else and discover some helpful resources blow.</p>
                 </div>
-                <div className='flex flex-wrap text-center'>
-                    <div className='mx-10'>
-                        <div className='bg-womenImg w-96 h-72 bg-cover shadow-lg'></div>
+                <div className='grid grid-col-1 gap-5 lg:grid-cols-3 text-center align-middle'>
+                    <div className='mb-7'>
+                        <div className='bg-womenImg bg-cover shadow-lg w-96 h-72'></div>
                         <h2 className='mt-5 mb-3 text-lg'>Need New Clothes?</h2>
                         <Link to='products/all' className='underline underline-offset-8 opacity-80 capitalize'>Shopping With Us</Link>
                     </div>
-                    <div className='mx-10'>
-                        <div className='bg-accessoriesImg w-96 h-72 bg-cover shadow-lg'></div>
+                    <div className='mb-7'>
+                        <div className='bg-accessoriesImg bg-cover shadow-lg w-96 h-72'></div>
                         <h2 className='mt-5 mb-3 text-lg'>Want to Know New Trend?</h2>
                         <Link to='/' className='underline underline-offset-8 opacity-80 capitalize'>Check Our Blog</Link>
                     </div>
-                    <div className='mx-10'>
-                        <div className='bg-banner w-96 h-72 bg-cover shadow-lg'></div>
+                    <div className='mb-7'>
+                        <div className='bg-banner bg-cover shadow-lg w-96 h-72'></div>
                         <h2 className='mt-5 mb-3 text-lg'>Need Help?</h2>
                         <Link to='/' className='underline underline-offset-8 opacity-80 capitalize'>Contact US</Link>
                     </div>
